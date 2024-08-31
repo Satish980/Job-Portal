515dffb980ee5020f25b2c86dfd798e011f054bd
satish980 <isatishkumar4008@gmail.com>
2024-08-31 23:31:41 +0530

    shadcn ui components added

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        Hello world <Button>Lgoinh</Button>
+      </div>
+    </>
+  )
+}
+
+export default App
