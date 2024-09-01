b8669caf01aa3c27bd97ebe21888b50747c685cf
satish980 <isatishkumar4008@gmail.com>
2024-09-01 17:37:52 +0530

    job listing api added with custom hook use-fetch

--- src/pages/job-listing.jsx
+++ src/pages/job-listing.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import { getJobs } from "@/api/apiJob";
+import useFetch from "@/hooks/use-fetch";
+import React, { useEffect } from "react";
 
 const JobListing = () => {
-  return (
-    <div>JobListing</div>
-  )
-}
+  const { fn: fnJobs, data: dataJobs, loading, error } = useFetch(getJobs, {});
+  console.log("==== data", dataJobs, loading, error);
+  useEffect(() => {
+    fnJobs();
+  }, []);
+  return <div>JobListing</div>;
+};
 
-export default JobListing
+export default JobListing;
